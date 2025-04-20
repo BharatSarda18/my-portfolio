d145322d94a80c5047ae235a0eb8e22cad18782d
Bharat-Sarda <bharat.sarda@lumiq.ai>
2025-04-21 01:13:03 +0530

    resume id changed

--- data/constants.js
+++ data/constants.js
@@ -6,8 +6,8 @@ export const socialMediaDetails={
     contaxctnumber:"+91 7627064727",
     location:"Noida, Uttar Pradesh, India",
     shortLocation:"Noida",
-    resumeDownloadLink:"https://drive.google.com/uc?export=download&id=1wDtY5VUzGK_HxwPO3qpvoh16lUg5bwUp",
-    resumeLink:"",
+    resumeDownloadLink:"https://drive.google.com/uc?export=download&id=1wswxXv7lS9FjQKB2L0RbVQGXQBZ9kDIg",
+    resumeLink:"https://drive.google.com/file/d/1wswxXv7lS9FjQKB2L0RbVQGXQBZ9kDIg/view",
     lastResumeUpdated:"April 2025",
     experiance:"3+ Years"
 }
